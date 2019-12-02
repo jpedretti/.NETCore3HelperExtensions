fd08dbbf497f31116a76bded8b4db91685664f90
Rafael Pedretti <Rafael.Pedretti@agoda.com>
2019-12-02 10:29:05 +0700

    Added sorted collections

--- Extensions/CollectionExtensions.cs
+++ Extensions/CollectionExtensions.cs
@@ -67,6 +67,62 @@ namespace RJPSoft.HelperExtensions
             return nonNull;
         }
 
+        public static LinkedList<T> FilterNotNull<T>(this LinkedList<T?> collection) where T : class
+        {
+            var nonNull = new LinkedList<T>();
+            foreach (var item in collection)
+
+            {
+                if (item is T valueOfT)
+                {
+                    nonNull.AddLast(valueOfT);
+                }
+            }
+            return nonNull;
+        }
+
+        public static LinkedList<T> FilterNotNull<T>(this LinkedList<T?> collection) where T : struct
+        {
+            var nonNull = new LinkedList<T>();
+            foreach (var item in collection)
+
+            {
+                if (item is T valueOfT)
+                {
+                    nonNull.AddLast(valueOfT);
+                }
+            }
+            return nonNull;
+        }
+
+        public static SortedList<U, T> FilterNotNull<U, T>(this SortedList<U, T?> collection) where T : class where U : notnull
+        {
+            var nonNull = new SortedList<U, T>(collection.Comparer);
+            foreach (var item in collection)
+
+            {
+                if (item.Value is T valueOfT)
+                {
+                    nonNull.Add(item.Key, valueOfT);
+                }
+            }
+            return nonNull;
+        }
+
+        public static SortedList<U, T> FilterNotNull<U, T>(this SortedList<U, T?> collection) where T : struct where U : notnull
+        {
+            var nonNull = new SortedList<U, T>(collection.Comparer);
+            foreach (var item in collection)
+
+            {
+                if (item.Value is T valueOfT)
+                {
+                    nonNull.Add(item.Key, valueOfT);
+                }
+            }
+            return nonNull;
+        }
+
         #endregion List
 
         #region Array
@@ -110,7 +166,7 @@ namespace RJPSoft.HelperExtensions
 
         public static HashSet<T> FilterNotNull<T>(this HashSet<T?> collection) where T : class
         {
-            var nonNull = new HashSet<T>();
+            var nonNull = new HashSet<T>(collection.Comparer);
             foreach (var item in collection)
             {
                 if (item is T valueOfT)
@@ -133,6 +189,60 @@ namespace RJPSoft.HelperExtensions
             }
             return nonNull;
         }
+
+        public static HashSet<T> FilterNotNull<T>(this HashSet<T?> collection, IEqualityComparer<T> comparer) where T : struct
+        {
+            var nonNull = new HashSet<T>(comparer);
+            foreach (var item in collection)
+            {
+                if (item is T valueOfT)
+                {
+                    nonNull.Add(valueOfT);
+                }
+            }
+            return nonNull;
+        }
+
+        public static SortedSet<T> FilterNotNull<T>(this SortedSet<T?> collection) where T : class
+        {
+            var nonNull = new SortedSet<T>(collection.Comparer);
+            foreach (var item in collection)
+            {
+                if (item is T valueOfT)
+                {
+                    nonNull.Add(valueOfT);
+                }
+            }
+            return nonNull;
+        }
+
+        public static SortedSet<T> FilterNotNull<T>(this SortedSet<T?> collection) where T : struct
+        {
+            var nonNull = new SortedSet<T>();
+            foreach (var item in collection)
+            {
+                if (item is T valueOfT)
+                {
+                    nonNull.Add(valueOfT);
+                }
+            }
+            return nonNull;
+        }
+
+        public static SortedSet<T> FilterNotNull<T>(this SortedSet<T?> collection, IComparer<T> comparer) where T : struct
+        {
+            var nonNull = new SortedSet<T>(comparer);
+            foreach (var item in collection)
+            {
+                if (item is T valueOfT)
+                {
+                    nonNull.Add(valueOfT);
+                }
+            }
+            return nonNull;
+        }
+
+
         #endregion Set
 
         #region Dictionary
@@ -163,7 +273,96 @@ namespace RJPSoft.HelperExtensions
             return nonNull;
         }
 
+        public static SortedDictionary<T, U> FilterNotNull<T, U>(this SortedDictionary<T, U?> collection) where T : notnull where U : class
+        {
+            var nonNull = new SortedDictionary<T, U>(collection.Comparer);
+            foreach (var item in collection)
+            {
+                if (item.Value is U valueOfU)
+                {
+                    nonNull.Add(item.Key, valueOfU);
+                }
+            }
+            return nonNull;
+        }
+
+        public static SortedDictionary<T, U> FilterNotNull<T, U>(this SortedDictionary<T, U?> collection) where T : notnull where U : struct
+        {
+            var nonNull = new SortedDictionary<T, U>(collection.Comparer);
+            foreach (var item in collection)
+            {
+                if (item.Value is U valueOfU)
+                {
+                    nonNull.Add(item.Key, valueOfU);
+                }
+            }
+            return nonNull;
+        }
+
         #endregion Dictionary
 
+        #region Queue
+
+        public static Queue<T> FilterNotNull<T>(this Queue<T?> collection) where T : class
+        {
+            var nonNull = new Queue<T>();
+            foreach (var item in collection)
+
+            {
+                if (item is T valueOfT)
+                {
+                    nonNull.Enqueue(valueOfT);
+                }
+            }
+            return nonNull;
+        }
+
+        public static Queue<T> FilterNotNull<T>(this Queue<T?> collection) where T : struct
+        {
+            var nonNull = new Queue<T>();
+            foreach (var item in collection)
+
+            {
+                if (item is T valueOfT)
+                {
+                    nonNull.Enqueue(valueOfT);
+                }
+            }
+            return nonNull;
+        }
+
+        #endregion
+
+        #region Stack
+
+        public static Stack<T> FilterNotNull<T>(this Stack<T?> collection) where T : class
+        {
+            var nonNull = new Stack<T>();
+            foreach (var item in collection)
+
+            {
+                if (item is T valueOfT)
+                {
+                    nonNull.Push(valueOfT);
+                }
+            }
+            return nonNull;
+        }
+
+        public static Stack<T> FilterNotNull<T>(this Stack<T?> collection) where T : struct
+        {
+            var nonNull = new Stack<T>();
+            foreach (var item in collection)
+
+            {
+                if (item is T valueOfT)
+                {
+                    nonNull.Push(valueOfT);
+                }
+            }
+            return nonNull;
+        }
+
+        #endregion
     }
 }
--- ExtensionsTests/CollectionExtensionsTest.cs
+++ ExtensionsTests/CollectionExtensionsTest.cs
@@ -1,6 +1,7 @@
 ﻿using FluentAssertions;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
 using System.Linq;
 using Xunit;
 
@@ -8,10 +9,12 @@ namespace RJPSoft.HelperExtensions
 {
     public class CollectionExtensionsTest
     {
+        #region IEnumerable
+
         [Fact(DisplayName = "IEnumerable: Flatten should remove null from collection of class")]
         public void CollectionExtensions_IEnumerable_Flatten_Should_RemoveNull_Class()
         {
-            var collection = new List<Uri?>() { new Uri("file://local"), null, new Uri("https://github.com") }.AsEnumerable();
+            var collection = new List<Uri>() { new Uri("file://local"), null, new Uri("https://github.com") }.AsEnumerable();
             var flattened = collection.FilterNotNull();
             flattened.Should().BeEquivalentTo(new Uri[] { new Uri("file://local"), new Uri("https://github.com") });
         }
@@ -27,7 +30,7 @@ namespace RJPSoft.HelperExtensions
         [Fact(DisplayName = "IEnumerable of class: ForEachNotNull should execute action only for not null values")]
         public void CollectionExtensions_IEnumerableOfClass_ForEachNotNull_SHouldExecuteActionOnlyForNoNullValues()
         {
-            var collection = new List<Uri?>() { new Uri("file://local"), null, new Uri("https://github.com") }.AsEnumerable();
+            var collection = new List<Uri>() { new Uri("file://local"), null, new Uri("https://github.com") }.AsEnumerable();
             var times = 0;
             var stringResult = new List<string>();
 
@@ -62,10 +65,14 @@ namespace RJPSoft.HelperExtensions
             intResult[1].Should().Be(2);
         }
 
+        #endregion
+
+        #region List
+
         [Fact(DisplayName = "List: Flatten should remove null from collection of class")]
         public void CollectionExtensions_List_Flatten_Should_RemoveNull_Class()
         {
-            var collection = new List<Uri?>() { new Uri("file://local"), null, new Uri("https://github.com") };
+            var collection = new List<Uri>() { new Uri("file://local"), null, new Uri("https://github.com") };
             var flattened = collection.FilterNotNull();
             flattened.Should().BeOfType<List<Uri>>();
             flattened.Should().Equal(new List<Uri>() { new Uri("file://local"), new Uri("https://github.com") });
@@ -80,10 +87,62 @@ namespace RJPSoft.HelperExtensions
             flattened.Should().Equal(new List<int>() { 1, 2, 4, 5 });
         }
 
+        [Fact(DisplayName = "LinkedList: Flatten should remove null from collection of class")]
+        public void CollectionExtensions_LinkedList_Flatten_Should_RemoveNull_Class()
+        {
+            var collection = new LinkedList<Uri>(new Uri[] { new Uri("file://local"), null, new Uri("https://github.com") });
+            var flattened = collection.FilterNotNull();
+            flattened.Should().BeOfType<LinkedList<Uri>>();
+            flattened.Should().BeEquivalentTo(
+                new LinkedList<Uri>(new Uri[] { new Uri("file://local"), new Uri("https://github.com") }),
+                options => options.WithStrictOrdering()
+            );
+        }
+
+        [Fact(DisplayName = "LinkedList: Flatten should remove null from collection of struct")]
+        public void CollectionExtensions_LinkedList_Flatten_Should_RemoveNull_Struct()
+        {
+            var collection = new LinkedList<int?>(new int?[] { 1, 2, null, 4, 5 });
+            var flattened = collection.FilterNotNull();
+            flattened.Should().BeOfType<LinkedList<int>>();
+            flattened.Should().BeEquivalentTo(
+                new LinkedList<int>(new int[] { 1, 2, 4, 5 }),
+                options => options.WithStrictOrdering()
+            );
+        }
+
+        [Fact(DisplayName = "SortedList: Flatten should remove null from collection of class")]
+        public void CollectionExtensions_SortedList_Flatten_Should_RemoveNull_Class()
+        {
+            var collection = new SortedList<string, Uri>() { { "3", new Uri("file://local") }, { "1", null }, { "2", new Uri("https://github.com") } };
+            var flattened = collection.FilterNotNull();
+            flattened.Should().BeOfType<SortedList<string, Uri>>();
+            flattened.Should().BeEquivalentTo(
+                new SortedList<string, Uri>() { { "3", new Uri("file://local") }, { "2", new Uri("https://github.com") } },
+                options => options.WithStrictOrdering()
+            );
+        }
+
+        [Fact(DisplayName = "SortedList: Flatten should remove null from collection of struct")]
+        public void CollectionExtensions_SortedList_Flatten_Should_RemoveNull_Struct()
+        {
+            var collection = new SortedList<string, int?>() { { "3", 3 }, { "1", null }, { "2", 2 } };
+            var flattened = collection.FilterNotNull();
+            flattened.Should().BeOfType<SortedList<string, int>>();
+            flattened.Should().BeEquivalentTo(
+                new SortedList<string, int?>() { { "3", 3 }, { "2", 2 } },
+                options => options.WithStrictOrdering()
+            );
+        }
+
+        #endregion
+
+        #region Array
+
         [Fact(DisplayName = "Array: Flatten should remove null from collection of class")]
         public void CollectionExtensions_Array_Flatten_Should_RemoveNull_Class()
         {
-            var collection = new Uri?[] { new Uri("file://local"), null, new Uri("https://github.com") };
+            var collection = new Uri[] { new Uri("file://local"), null, new Uri("https://github.com") };
             var flattened = collection.FilterNotNull();
             flattened.Should().BeOfType<Uri[]>();
             flattened.Should().Equal(new Uri[] { new Uri("file://local"), new Uri("https://github.com") });
@@ -98,10 +157,14 @@ namespace RJPSoft.HelperExtensions
             flattened.Should().Equal(new int[] { 1, 2, 4, 5 });
         }
 
+        #endregion
+
+        #region Set
+
         [Fact(DisplayName = "HashSet: Flatten should remove null from collection of class")]
         public void CollectionExtensions_HashSet_Flatten_Should_RemoveNull_Class()
         {
-            var collection = new HashSet<Uri?>() { new Uri("file://local"), null, new Uri("https://github.com") };
+            var collection = new HashSet<Uri>() { new Uri("file://local"), null, new Uri("https://github.com") };
             var flattened = collection.FilterNotNull();
             flattened.Should().BeOfType<HashSet<Uri>>();
             flattened.Should().Equal(new HashSet<Uri>() { new Uri("file://local"), new Uri("https://github.com") });
@@ -116,13 +179,71 @@ namespace RJPSoft.HelperExtensions
             flattened.Should().Equal(new HashSet<int>() { 1, 2, 4, 5 });
         }
 
+        [Fact(DisplayName = "HashSet with comparer: Flatten should remove null from collection of struct")]
+        public void CollectionExtensions_HashSet_Comparer_Flatten_Should_RemoveNull_Struct()
+        {
+            var collection = new HashSet<MyStruct?>(new MyStructEqualityCompararNullable()) {
+                new MyStruct { Id = 1 },
+                new MyStruct { Id = 2 },
+                new MyStruct { Id = 1 },
+                null,
+                new MyStruct { Id = 4 },
+                new MyStruct { Id = 5 } };
+            var flattened = collection.FilterNotNull(new MyStructEqualityComparar());
+            flattened.Should().BeOfType<HashSet<MyStruct>>();
+            flattened.Should().Equal(new HashSet<MyStruct>() { new MyStruct { Id = 1 }, new MyStruct { Id = 2 }, new MyStruct { Id = 4 }, new MyStruct { Id = 5 } });
+        }
+
+        [Fact(DisplayName = "SortedSet: Flatten should remove null from collection of class")]
+        public void CollectionExtensions_SortedSet_Flatten_Should_RemoveNull_Class()
+        {
+            var comparer = new UriComparer();
+            var collection = new SortedSet<Uri>(comparer) { new Uri("file://local"), null, new Uri("https://github.com") };
+            var flattened = collection.FilterNotNull();
+            flattened.Should().BeOfType<SortedSet<Uri>>();
+            flattened.Should().Equal(new SortedSet<Uri>(comparer) { new Uri("file://local"), new Uri("https://github.com") });
+        }
+
+        [Fact(DisplayName = "SortedSet: Flatten should remove null from collection of struct")]
+        public void CollectionExtensions_SortedSet_Flatten_Should_RemoveNull_Struct()
+        {
+            var collection = new SortedSet<int?> { 1, 2, null, 4, 5 };
+            var flattened = collection.FilterNotNull();
+            flattened.Should().BeOfType<SortedSet<int>>();
+            flattened.Should().Equal(new SortedSet<int>() { 1, 2, 4, 5 });
+        }
+
+        [Fact(DisplayName = "SortedSet wit comparer: Flatten should remove null from collection of struct")]
+        public void CollectionExtensions_SortedSet_Comparer_Flatten_Should_RemoveNull_Struct()
+        {
+            var collection = new SortedSet<MyStruct?>(new MyStructCompararNullable()) {
+                new MyStruct { Id = 1 }, new MyStruct { Id = 2 },
+                new MyStruct { Id = 1 },
+                null,
+                new MyStruct { Id = 4 }, new MyStruct { Id = 5 }
+            };
+
+            var flattened = collection.FilterNotNull(new MyStructComparar());
+            flattened.Should().BeOfType<SortedSet<MyStruct>>();
+            flattened.Should().Equal(new SortedSet<MyStruct>(new MyStructComparar()) {
+                new MyStruct { Id = 1 },
+                new MyStruct { Id = 2 },
+                new MyStruct { Id = 4 },
+                new MyStruct { Id = 5 }
+            });
+        }
+
+        #endregion
+
+        #region Dictionary
+
         [Fact(DisplayName = "Dictionary: Flatten should remove null from collection of class")]
         public void CollectionExtensions_Dictionary_Flatten_Should_RemoveNull_Class()
         {
-            var collection = new Dictionary<int, Uri?>() { { 1, new Uri("file://local") }, { 2, null }, { 3, new Uri("https://github.com") } };
+            var collection = new Dictionary<int, Uri>() { { 1, new Uri("file://local") }, { 2, null }, { 3, new Uri("https://github.com") } };
             var flattened = collection.FilterNotNull();
             flattened.Should().BeOfType<Dictionary<int, Uri>>();
-            flattened.Should().BeEquivalentTo(new Dictionary<int, Uri?>() { { 1, new Uri("file://local") }, { 3, new Uri("https://github.com") } });
+            flattened.Should().BeEquivalentTo(new Dictionary<int, Uri>() { { 1, new Uri("file://local") }, { 3, new Uri("https://github.com") } });
         }
 
         [Fact(DisplayName = "Dictionary: Flatten should remove null from collection of struct")]
@@ -132,6 +253,58 @@ namespace RJPSoft.HelperExtensions
             var flattened = collection.FilterNotNull();
             flattened.Should().BeOfType<Dictionary<string, int>>();
             flattened.Should().BeEquivalentTo(new Dictionary<string, int>() { { "1", 1 }, { "3", 3 } });
+        }
+
+        #endregion
+
+        class UriComparer : IComparer<Uri>
+        {
+            public int Compare([AllowNull] Uri x, [AllowNull] Uri y)
+            {
+                if (x != null && y != null)
+                {
+                    return x.ToString().CompareTo(y.ToString());
+                }
+
+                return x != null ? 1 : -1;
+            }
+        }
+
+        struct MyStruct
+        {
+            public int Id { get; set; }
+        }
+
+        struct MyStructCompararNullable : IComparer<MyStruct?>
+        {
+            public int Compare([AllowNull] MyStruct? x, [AllowNull] MyStruct? y)
+            {
+                if (x is MyStruct vX && y is MyStruct vY)
+                {
+                    return vX.Id.CompareTo(vY.Id);
+                }
+
+                return x != null ? 1 : -1;
+            }
+        }
+
+        struct MyStructComparar : IComparer<MyStruct>
+        {
+            public int Compare([AllowNull] MyStruct x, [AllowNull] MyStruct y) => x.Id.CompareTo(y.Id);
+        }
+
+        struct MyStructEqualityCompararNullable : IEqualityComparer<MyStruct?>
+        {
+            public bool Equals([AllowNull] MyStruct? x, [AllowNull] MyStruct? y) => x?.Id == y?.Id;
+
+            public int GetHashCode([DisallowNull] MyStruct? obj) => obj.Value.Id;
+        }
+
+        struct MyStructEqualityComparar : IEqualityComparer<MyStruct>
+        {
+            public bool Equals([AllowNull] MyStruct x, [AllowNull] MyStruct y) => x.Id == y.Id;
+
+            public int GetHashCode([DisallowNull] MyStruct obj) => obj.Id;
         }
     }
 }
